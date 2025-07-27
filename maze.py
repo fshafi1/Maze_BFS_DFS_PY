adab36f29f4481e180cfa43c02f641bc2bdce0f8
fshafi1 <fshafi1@umbc.edu>
2025-07-27 10:40:28 -0400

    Getting started

---
+++ maze.py
@@ -0,0 +1,13 @@
+
+
+class Node():
+    def __init__(self, state, action, parent):
+        self.state = state
+        self.action = action
+        self.parent = parent
+
+
+
+if __name__ == "__main__":
+    
+        pass
